606fa274cbef615972865c3e71edd8a56e35cb09
Henry Lee <henryleeworld@gmail.com>
2023-11-30 00:11:42 +0800

    feat:更新最新資料

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -97,7 +97,19 @@
             </nav>
             <div class="container-fluid py-4">
                 @yield('content')
-				@include('layouts.footer')
+                <footer class="footer py-4  ">
+                    <div class="container-fluid">
+                        <div class="row align-items-center justify-content-lg-between">
+                            <div class="col-lg-6 mb-lg-0 mb-4">
+                                <div class="copyright text-center text-sm text-muted text-lg-start">
+                                    © <script>
+                                    document.write(new Date().getFullYear())
+                                    </script> {{ __('by Henry Lee') }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
             </div>
         </main>
         <!-- Scripts -->
